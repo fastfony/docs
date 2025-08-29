380128cb73d343861255103c937da5d20d7b40ad
neothone (Mathieu Dumoutier) <13251143+neothone@users.noreply.github.com>
2025-08-29 23:02:42 +0200

    feat: add sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://docs.fastfony.com',
+  generateRobotsTxt: true, // Génère aussi le robots.txt
+  // Optionnel : exclure des pages, configurer les priorités, etc.
+}
