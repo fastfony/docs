24b8ed1820f4e21e1ebe95ea72b9fbee965387a8
neothone (Mathieu Dumoutier) <13251143+neothone@users.noreply.github.com>
2026-01-10 21:38:27 +0100

    npm update & generate static page

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 import nextra from 'nextra'
 
 const withNextra = nextra({
-  search: {
-    codeblocks: false
-  }
 })
 
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
   // ... Other Next.js config options
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 })
